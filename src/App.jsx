e0b7dc2a1c4f6cad1ff43969d333bc7900b815b8
saichandangorli1 <gorlisaichandan14@gmail.com>
2024-02-28 15:54:35 +0530

    adding shadcn ui

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+import { Login } from './comp/Login'
+const App = () => {
+
+  return (
+
+    <>
+    <div className="container flex justify-center items-center h-full my-3">
+    <Login />
+    </div>
+    {/* <div className="container flex justify-center items-center h-full">
+    <Button className='bg-black text-white hover:bg-gray-800 hover:text-white rounded-xl'>Click Me</Button>
+    </div> */}
+    </>
+  )
+}
+
+export default App
